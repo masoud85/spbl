b6909f399628d5c24446fbfe85a653a8ab0f57d0
ahadi.masoud <masoud.ahadi@gmail.com>
2020-04-05 15:01:57 +0430

    Initial commit for our spring boot Reddit Clone!

---
+++ src/test/java/com/osveh/springbootlearning/SpringBootLearningApplicationTests.java
@@ -0,0 +1,15 @@
+package com.osveh.springbootlearning;
+/*
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+*/
