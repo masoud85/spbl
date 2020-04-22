af5a0dffc15e364aa225e8adc7a0e91407980d22
ahadi.masoud <masoud.ahadi@gmail.com>
2020-04-22 09:06:33 +0430

    Adding and changing pages in order to show existing links and adding new link

--- src/main/java/com/osveh/SpringBootLearningApplication.java
+++ src/main/java/com/osveh/SpringBootLearningApplication.java
@@ -1,18 +1,14 @@
 package com.osveh;
 
 import com.osveh.config.SpringBootLearningConfig;
-import com.osveh.domain.Comment;
-import com.osveh.domain.Link;
-import com.osveh.repository.CommentRepository;
-import com.osveh.repository.LinkRepository;
+import org.ocpsoft.prettytime.PrettyTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -29,17 +25,9 @@ public class SpringBootLearningApplication {
         SpringApplication.run(SpringBootLearningApplication.class, args);
     }
 
-    //    @Bean
-    @Profile("dev")
-    CommandLineRunner commandLineRunner(LinkRepository linkRepository, CommentRepository commentRepository) {
-        return (args -> {
-            Link link = new Link("Getting started with spring boot 2", "https://spring.io/projects/spring-boot");
-            linkRepository.save(link);
-
-            Comment comment = new Comment("This spring boot 2 link is awesome", link);
-            commentRepository.save(comment);
-            link.addComment(comment);
-        });
+    @Bean
+    public PrettyTime prettyTime() {
+        return new PrettyTime();
     }
 
 }
--- src/main/java/com/osveh/controller/LinkController.java
+++ src/main/java/com/osveh/controller/LinkController.java
@@ -2,14 +2,23 @@ package com.osveh.controller;
 
 import com.osveh.domain.Link;
 import com.osveh.repository.LinkRepository;
-import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
+import javax.validation.Valid;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/links")
+@Controller
 public class LinkController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LinkController.class);
 
     private LinkRepository linkRepository;
 
@@ -18,29 +27,42 @@ public class LinkController {
     }
 
     @GetMapping("/")
-    public List<Link> links() {
-        return linkRepository.findAll();
+    public String list(Model model) {
+        model.addAttribute("links", linkRepository.findAll());
+        return "link/list";
     }
 
-    @PostMapping("/create")
-    public Link create(@ModelAttribute Link link) {
-        return linkRepository.save(link);
+    @GetMapping("/link/{linkId}")
+    public String read(@PathVariable Long linkId, Model model) {
+        Optional<Link> link = linkRepository.findById(linkId);
+        if (link.isPresent()) {
+            model.addAttribute("link", link.get());
+            model.addAttribute("success", model.containsAttribute("success"));
+            return "link/view";
+        } else {
+            return "redirect:/";
+        }
     }
 
-    @GetMapping("/{linkId}")
-    public Optional<Link> read(@PathVariable Long linkId) {
-        return linkRepository.findById(linkId);
-    }
-
-    @PutMapping("/{linkId}}")
-    public Link update(@PathVariable Long linkId, @ModelAttribute Link link) {
-        return linkRepository.save(link);
-    }
-
-    @DeleteMapping("/{linkId}}")
-    public void delete(@PathVariable Long linkId) {
-        linkRepository.deleteById(linkId);
+    @GetMapping("/link/submit")
+    public String newLinkForm(Model model) {
+        model.addAttribute("link", new Link());
+        return "link/submit";
     }
 
 
+    @PostMapping("/link/submit")
+    public String createLink(@Valid Link link, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            LOGGER.info("Validation errors were found while submitting a new link.");
+            model.addAttribute("link", link);
+            return "link/submit";
+        } else {
+            // save our link
+            linkRepository.save(link);
+            LOGGER.info("Our link was saved successfully");
+            redirectAttributes.addAttribute("id", link.getId()).addFlashAttribute("success", true);
+            return "redirect:/link/{id}";
+        }
+    }
 }
